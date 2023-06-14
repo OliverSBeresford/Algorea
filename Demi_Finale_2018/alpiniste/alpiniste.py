70d89497f8b9bfcef45c20fd440d1fb4438d0125
Oliver Beresford <oggynaut@icloud.com>
2023-06-13 21:56:16 -0400

    Added comments to alpiniste.py (in English)

--- Demi_Finale_2018/alpiniste/alpiniste.py
+++ Demi_Finale_2018/alpiniste/alpiniste.py
@@ -1,11 +1,19 @@
-nbnombres = int(input())
-num = 0
-num1 = int(input())
-for i in range(nbnombres - 1):
-    num2 = int(input())
-    if num1 < num2:
-        num += (num2 - num1) * 2
-    elif num1 > num2:
-        num += num1 - num2
-    num1 = num2
-print(num)
+def main():
+    nbnombres = int(input())   # Read the number of input values
+    num = 0                   # Initialize the total to 0
+    num1 = int(input())       # Read the first input value
+
+    for i in range(nbnombres - 1):
+        num2 = int(input())   # Read the next input value
+
+        # Compare the previous value (num1) with the current value (num2)
+        if num1 < num2:
+            num += (num2 - num1) * 2   # If num2 is greater, add the difference multiplied by 2 to the total
+        elif num1 > num2:
+            num += num1 - num2         # If num1 is greater, add the difference to the total
+
+        num1 = num2   # Set the current value as the new previous value for the next iteration
+
+    print(num)   # Print the calculated total
+
+main()

5bcfd873a7f9eabdc298a6e013bc7dbdda93c94f
Oliver Beresford <oggynaut@icloud.com>
2025-02-26 22:27:33 -0500

    Stopped using numpy and checking to see if square checking is pointless to save time

--- FranceIOI_4/camping.py
+++ FranceIOI_4/camping.py
@@ -1,4 +1,4 @@
-import numpy as np
+import sys
 
 """
 
@@ -44,16 +44,25 @@ output:
 #         square_size += 1
     
 #     return square_size
-def area(grid, start_row, start_col):
-    rows, cols = np.shape(grid)
+def area(grid, start_row, start_col, max_area):
+    rows, cols = len(grid), len(grid[0])
     
     square_size = 1
     
+    if start_row < 0 or start_row + max_area >= rows or start_col < 0 or start_col + max_area >= cols:
+        return 0
+    
+    if any([row[start_col + max_area] for row in grid[start_row:start_row + max_area + 1]]):
+        return 0
+            
+    if any([col for col in grid[start_row + max_area][start_col:start_col + max_area]]):
+        return 0
+    
     while start_row >= 0 and start_row + square_size < rows and start_col >= 0 and start_col + square_size < cols:
-        if np.any(grid[start_row:start_row + square_size + 1, start_col + square_size]):
+        if any([row[start_col + square_size] for row in grid[start_row:start_row + square_size + 1]]):
             return square_size
             
-        if np.any(grid[start_row + square_size, start_col:start_col + square_size]):
+        if any([col for col in grid[start_row + square_size][start_col:start_col + square_size]]):
             return square_size
                 
         square_size += 1
@@ -61,14 +70,14 @@ def area(grid, start_row, start_col):
     return square_size
 
 def main():
-    rows, cols = map(int, input().split())
-    grid = np.array([list(map(int, input().split())) for _ in range(rows)])
+    rows, cols = map(int, sys.stdin.readline().split())
+    grid = [list(map(int, sys.stdin.readline().split())) for _ in range(rows)]
     max_area = 0
     
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 0:
-                max_area = max(max_area, area(grid, row, col))
+                max_area = max(max_area, area(grid, row, col, max_area))
                 
     print(max_area)
     

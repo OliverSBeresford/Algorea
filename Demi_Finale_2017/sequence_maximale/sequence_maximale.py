c6a4e816cc71f866cb83ecc2a55966c7c5bf884d
Oliver Beresford <oggynaut@icloud.com>
2025-02-28 10:32:54 -0500

    Update sequence_maximale so that it is O(n) instead of O(n^2)

--- Demi_Finale_2017/sequence_maximale/sequence_maximale.py
+++ Demi_Finale_2017/sequence_maximale/sequence_maximale.py
@@ -1,35 +1,39 @@
-def main():
-    nbnombres = int(input())
-    currentnum = 0
-    previous = 0
-    temp = 0
-    lenprev = 0
-    lencur = 0
-    lentemp = 0
-    i = 0
-    max = 0
-    while i < nbnombres:
-        new = int(input())
-        if currentnum < new:
-            previous = currentnum
-            currentnum = new
-            lencur += 1
-        else:
-            i += 1
-            while i < nbnombres:
-                x = int(input())
-                if temp < x:
-                    temp = x
-                    lentemp += 1
-                else:
-                    break
-                i += 1
-            
-            if lentemp > lenprev:
-                lenprev = lencur
-                lencur = lentemp
-                lentemp = 0
+import sys
 
-        i += 1
-    print(lencur + lenprev)
+def main():
+    sys.stdin = open("input.txt", "r")
+    
+    length = int(input())
+    
+    numbers = [int(input()) for _ in range(length)]
+    lengths = [0] * length
+    
+    max_length = 1
+    window = length - 1
+    current = 0
+    
+    for i in range(length):
+        if i == 0:
+            lengths[i] = 1
+        elif numbers[i] > numbers[i-1]:
+            lengths[i] = lengths[i-1] + 1
+        else:
+            lengths[i] = 1
+    
+    while window >= 0:
+        current = lengths[window]
+        
+        if current > max_length:
+            max_length = current
+        
+        while lengths[window] != 1:
+            window -= 1
+        
+        if window - 2 >= 0 and numbers[window - 2] < numbers[window] and lengths[window - 2] + current > max_length:
+            max_length = current + lengths[window - 2]
+        
+        window -= 1
+    
+    print(max_length)
+            
 main()
